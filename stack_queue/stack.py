2bcf7aa29990e1b3dbb852840873eca155e4d231
Jiyun Kim <yeon520111@daum.net>
2022-01-18 09:12:19 +0900

    first commit

--- stack_queue/stack.py
+++ stack_queue/stack.py
@@ -1,6 +1,5 @@
 
-stack=[]
-
+stack=[]  #list 자료형 이용
 #push
 stack.append(2)
 stack.append(4)
@@ -22,6 +21,13 @@ print('후입선출 pop-2 :',stack)
 
 stack.pop()
 print('후입선출 pop-3 :',stack)
+
+stack.append(3)
+stack.append(2)
+stack.append(1)
+print("append 함수 이용 요소 넣기: ",stack)
+print(stack[::-1]) #최상단 원소부터 출력 #리스트 뒤집어서 출력함(먼저 나가고자 하는 원소부터 출력)
+print(stack) #최하단 원소부터 출력 #먼저 들어온 요소부터 출력
 
 print("stack_char:",stack_char)
 print('후입선출 pop : ',stack_char.pop()+ ' stack_char :',stack_char)
